f9ffbaafb2f9f82ac15729553746821c27c38cac
Julia <samoletsk8@yandex.ru>
2026-03-27 16:29:33 +0300

    fix import

--- src/test/java/ru/samokat/yandex/OrderCreationTest.java
+++ src/test/java/ru/samokat/yandex/OrderCreationTest.java
@@ -1,6 +1,5 @@
 package ru.samokat.yandex;
 
-import io.qameta.allure.Description;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
